3a4871b8331b09248fba2910439e5e6be6569b9c
thelastkiss113 <76501819+thelastkiss113@users.noreply.github.com>
2021-03-14 19:01:00 -0700

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-       Weather App
+       <h1>Weather App</h1>
+       <footer>
+         This Project was coded by <a href="https://www.jessicablum.com">Jessica Blum</a> and is {" "} 
+       <a href= "https://github.com/thelastkiss113/react-app"
+       target="_blank"> open-sourced by GitHub
+       </a>
+       </footer>
     </div>
   );
 }
 
-export default App;
