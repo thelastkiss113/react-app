014cb912376d18d3e0875aff56b06cd244d2333c
thelastkiss113 <76501819+thelastkiss113@users.noreply.github.com>
2021-03-17 14:15:35 -0700

    Updated API Integration

---
+++ src/WeatherForecast.js
@@ -0,0 +1,34 @@
+  
+import React, { useState } from "react";
+import WeatherForecastPreview from "./WeatherForecastPreview";
+import axios from "axios";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function handleForecastResponse(response) {
+    setForecast(response.data);
+    setLoaded(true);
+  }
+
+  if (loaded && props.city === forecast.city.name) {
+    return (
+      <div className="WeatherForecast row">
+        <WeatherForecastPreview data={forecast.list[0]} />
+        <WeatherForecastPreview data={forecast.list[1]} />
+        <WeatherForecastPreview data={forecast.list[2]} />
+        <WeatherForecastPreview data={forecast.list[3]} />
+        <WeatherForecastPreview data={forecast.list[4]} />
+        <WeatherForecastPreview data={forecast.list[5]} />
+      </div>
+    );
+  } else {
+    let apiKey = "489fc3d59538179eadeb506c770367a6";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleForecastResponse);
+
+    return null;
+  }
+}
