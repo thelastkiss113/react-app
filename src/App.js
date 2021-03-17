2e1e57b7013c47cf47d74580397c2b93001627d1
thelastkiss113 <76501819+thelastkiss113@users.noreply.github.com>
2021-03-17 15:27:00 -0700

    Improved Styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York"/>
+
        <footer>
          This Project was coded by <a href="https://www.jessicablum.com">Jessica Blum</a> and is {" "} 
        <a href= "https://github.com/thelastkiss113/react-app"
--- src/Weather.js
+++ src/Weather.js
@@ -56,7 +56,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-primary w-100"
+                className="btn btn-outline-light w-100"
               />
             </div>
           </div>
